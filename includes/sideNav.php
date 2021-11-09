d0635f10d0ce0cf77cd8232adebd7e9d3176ee0a
Gennifire <Jenniferdee86@gmail.com>
2021-11-09 15:42:31 +0000

    added slider and gallery pages

--- includes/sideNav.php
+++ includes/sideNav.php
@@ -2,9 +2,9 @@
 
 <nav class="sideNav">
     <div id="mySidenav" class="sidenav">
-        <a href="sidePage/tabs/About.html" id="about">About</a>
-        <a href="sidePage/Media/Gallery.html" id="gallery">Directions</a>
+        <a href="#" id="about">About</a>
+        <a href="#" id="gallery">Directions</a>
         <a href="#" id="projects">Meet</a>
-        <a href="sidePage/Quizzes/Contact.html" id="contact">Contact</a>
+        <a href="#" id="contact">Contact</a>
     </div>
 </nav>
